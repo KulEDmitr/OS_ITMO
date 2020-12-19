8287ae2277e830c4362e6738362f158100ddb3ab
KulEDmitr <37519644+KulEDmitr@users.noreply.github.com>
2020-12-20 02:07:56 +0300

    first lab's version added

---
+++ lab6/task3.bat
@@ -0,0 +1,6 @@
+net start > C:\lab6\services.txt
+net stop dnscache
+timeout /T 10
+net start > C:\lab6\servicesUpdate.txt
+fc C:\lab6\services.txt C:\lab6\servicesUpdate.txt > C:\lab6\servicesDiff.txt
+net start dnscache
---
+++ lab6/task4.bat
@@ -0,0 +1,2 @@
+driverquery /fo table /nh > C:\lab6\DRIVERS.txt
+sort /r C:\lab6\DRIVERS.txt /o C:\lab6\sortDRIVERS.txt
